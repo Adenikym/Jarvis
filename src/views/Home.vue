859e17886ac8dae52bc88589f842c0260ef0c0aa
Adenikym <65138512+Adenikym@users.noreply.github.com>
2021-02-17 01:22:26 +0100

    page one

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="home">
-  
+  <div class="home col-lg-12 py-0" style="">
+  <div class="select">
+    <div class="header d-flex ">
+      <img src="../assets/credit-card.svg" width="40px" alt="">
+<p >Jarvis Pay</p>
+    </div>
+
+    <div class="select-box col-lg-6 offset-lg-3 py-1">
+      <h2 class="text-center mt-2">Sign Up</h2>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -15,3 +24,19 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.home{
+  background: #E87312;
+  height: 100vh;
+
+}
+
+.select-box{
+  background-color: white;
+}
+
+.header{
+color:white;
+}
+</style>
