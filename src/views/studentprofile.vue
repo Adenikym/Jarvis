f9f9c5fd8b245a04c5358d0be25a72f6201c7556
Adenikym <mm5adeniky@gmail.com>
2021-02-24 15:12:22 +0100

    Update studentprofile.vue

--- src/views/studentprofile.vue
+++ src/views/studentprofile.vue
@@ -6,10 +6,22 @@
 
 <div class="heading d-flex col-lg-3">
     <rectangle class="mt-2" ></rectangle>
-    <h1 class="mt-3 ml-2" >Student Profile</h1>
+    <h1 class="mt-3 ml-2" >Student Profile</h1>    
 </div>
 
+<div class="student-details col-lg-6 offset-lg-3 p-4 mt-4">
+    <div class="name d-flex mt-4">
+        <p class="col-lg-4 mt-4">Name</p> <p class="offset-lg-4 mt-4">Oguntade Muhammad</p>
+    </div>
 
+       <div class="name d-flex mt-4">
+        <p class="col-lg-4 ">Email</p> <p class="offset-lg-4">Oguntade@gmail.com</p>
+    </div>
+
+       <div class="name d-flex mt-4">
+        <p class="col-lg-4 ">Phone Number</p> <p class="offset-lg-4">997-998-997</p>
+    </div>
+</div>
 </div>
 
 
@@ -38,5 +50,10 @@ export default {
 .heading h1{
     color: #0E4870;
 font-size:1.9rem;
+}
+.student-details{
+    background-color: #e5e5e5;
+    color:black;
+    height: 350px;
 }
 </style>
